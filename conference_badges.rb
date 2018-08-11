283c790ed7bbf4d1efb04d15257122a6f0b76994
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-08-11 00:58:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  new_attendees = []
+  attendees.each do |attendee|
+    new_attendees << "Hello, my name is #{attendee}."
+  end
+  new_attendees
+end
+
+def assign_rooms(attendees)
+  att
+end
